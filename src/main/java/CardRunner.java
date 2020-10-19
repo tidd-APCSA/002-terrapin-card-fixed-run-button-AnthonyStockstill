2093e513516944bee0a62e25b678e9f6e6d4b732
AnthonyStockst1 <hydropenguin1@gmail.com>
2020-10-19 15:03:42 +0000

    restarting

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,24 +1,6 @@
 public class CardRunner {
     public static void main(String args[]){
 
-      TerrapinCard billy = new TerrapinCard(50);
-      TerrapinCard brian = new TerrapinCard(50);
-
-      billy.payGourmet();
-      brian.payEconomical();
-      System.out.println("Billy: " + billy);
-      System.out.println("Brian: " + brian);
-
-      billy.loadMoney(28);
-      brian.payGourmet();
-      System.out.println("Billy: " + billy);
-      System.out.println("Brian: " + brian);
-
-      billy.payGourmet();
-      billy.payGourmet();
-      brian.loadMoney(50);
-      System.out.println("Billy: " + billy);
-      System.out.println("Brian: " + brian);
-
+    
     }
 }
--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -3,47 +3,6 @@ public class TerrapinCard {
   
   
   
-  
-  private double balance;
-  public TerrapinCard(double itsBalance){
-    balance = itsBalance; 
-  }
-  public String toString(){
-      return "The card has " + balance + " dollars";
-    }
-
-
-  public void payEconomical(){
-    if (balance>2.50){
-      balance-=2.50;
-    }
-    else {
-      balance += 0 ;
-    }
-  }
-
-
-  public void payGourmet(){
-    if (balance>4.00){
-      balance -= 4.00;
-    }
-    else {
-      balance += 0;
-    }
-  }
-
-  public void loadMoney(double amount){
-
-    if (balance+amount>150){
-      balance = 150;
-    }
-    else if(balance>0){
-      balance+=amount;
-    }
-    else {
-      balance+=0;
-    }
-  }
 
 
 }
