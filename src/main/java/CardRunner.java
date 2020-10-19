e1bf7b137aec3cf76f2366ef2f94496d206de28b
AnthonyStockst1 <hydropenguin1@gmail.com>
2020-10-19 15:18:11 +0000

    Step 3

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,13 +1,12 @@
 public class CardRunner {
     public static void main(String args[]){
-        TerrapinCard card = new TerrapinCard(50);
+        TerrapinCard card = new TerrapinCard(5);
         System.out.println(card);
 
-        card.payEconomical();
+        card.payGourmet();
         System.out.println(card);
         
         card.payGourmet();
-        card.payEconomical();
         System.out.println(card);
     }
 }
--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -4,10 +4,20 @@ public class TerrapinCard {
         balance = balanceAtStart;
   }
   public void payEconomical(){
+    if(balance >=2.50){
     balance -= 2.50;
+    }else{
+      balance -= 0;
+    }
   }
   public void payGourmet(){
-    balance -= 4;
+    if (balance >= 4){
+
+      balance -= 4;
+    }else{
+      balance -= 0;
+    }
+    
   }
   public String toString(){
       return "The card has " + balance + " dollars";
