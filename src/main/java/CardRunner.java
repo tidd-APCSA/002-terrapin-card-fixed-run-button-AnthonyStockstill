1078f76486de4b8a44acc13bdedcd19da3736339
AnthonyStockst1 <hydropenguin1@gmail.com>
2020-10-19 15:09:24 +0000

    Step 1

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,6 +1,7 @@
 public class CardRunner {
     public static void main(String args[]){
-
+        TerrapinCard card = new TerrapinCard(50);
+        System.out.println(card);
     
     }
 }
--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,5 +1,11 @@
 public class TerrapinCard {
-  
+  private double balance;
+  public TerrapinCard(double balanceAtStart){
+        balance = balanceAtStart;
+  }
+  public String toString(){
+      return "The card has " + balance + " dollars";
+  }
   
   
   
