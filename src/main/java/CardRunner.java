3bd6df69bf2c2b778d50d08cd6ed9a8a31fc7f94
AnthonyStockst1 <hydropenguin1@gmail.com>
2020-10-19 14:49:50 +0000

    the whole thing i think?

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,7 +1,24 @@
 public class CardRunner {
     public static void main(String args[]){
-        // ☕
-        System.out.println("hey!!!!!!");
+
+      TerrapinCard cardBilly = new TerrapinCard(50);
+      TerrapinCard cardBrian = new TerrapinCard(50);
+
+      cardBilly.payGourmet();
+      cardBrian.payEconomical();
+      System.out.println("Billy; " + cardBilly);
+      System.out.println("Brian; " + cardBrian);
+
+      cardBilly.loadMoney(28);
+      cardBrian.payGourmet();
+      System.out.println("Billy; " + cardBilly);
+      System.out.println("Brian; " + cardBrian);
+
+      cardBilly.payGourmet();
+      cardBilly.payGourmet();
+      cardBrian.loadMoney(50);
+      System.out.println("Billy; " + cardBilly);
+      System.out.println("Brian; " + cardBrian);
 
     }
 }
